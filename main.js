07a31fdab7f99e1a2f63b3fe8358084427714bdc
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-28 10:26:03 +0600

    cashout section created

--- main.js
+++ main.js
@@ -25,3 +25,45 @@ document.getElementById('btn-add-money')
         alert('Your number is not correct')
     }
 })
+
+
+// transection switcher section 
+document.getElementById('add-money-box')
+.addEventListener('click', function(){
+    document.getElementById('add-money-container').style.display= 'block';
+    document.getElementById('cashout-container').style.display= 'none';
+
+})
+document.getElementById('cashout-box')
+.addEventListener('click', function(){
+    document.getElementById('add-money-container').style.display= 'none';
+    document.getElementById('cashout-container').style.display= 'block';
+
+})
+
+
+// Cashout transection section 
+document.getElementById('btn-withdraw')
+.addEventListener('click', function(event){
+    event.preventDefault();
+    const agentNumber = document.getElementById('agent-number').value;
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+
+    const cashoutPin = document.getElementById('cashout-pin').value;
+    const convertedCashoutPin = parseInt(cashoutPin); 
+
+    if (agentNumber.length === 11){
+        if(convertedCashoutPin === 1234){
+            const balance = document.getElementById('balance').innerText;
+            const substract = balance - convertedAmount;
+            document.getElementById('balance').innerText = substract 
+        }
+        else{
+            alert("Your Pin is not Correct")
+        }
+    }
+    else{
+        alert('Your Number is not Correct')
+    }
+})
