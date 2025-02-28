2a5060a3bd2bec70a1770b3c17e0146e03a58448
freelancer-rased <mdraseduzzamanrased8707@gmail.com>
2025-02-28 15:43:55 +0600

    fix the issue

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ document.getElementById("btn-login").addEventListener("click", function (event) 
 
     if(accountNumber.length === 11){
         if(convertedPin === 1234){
-            window.location.href = './index.html'
+            window.location.href = './main.html'
         }
         else{
             alert('Your Pin is not correct')
